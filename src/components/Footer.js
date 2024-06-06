267c9d0e686cc2dacf1c5832379dad974534953a
Anup <anuprijal321@gmail.com>
2024-06-06 18:28:27 +0545

    added alt attribute in img tag in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,9 @@ function Footer() {
                 <Link to="https://github.com/AnupRijal123/React_Movie_App/tree/development-branch">
                     <h3>Get code</h3>
                 </Link>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCOSzabMLHpRMMuS289T6E0aC_ysnrhs6xrA&s" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCOSzabMLHpRMMuS289T6E0aC_ysnrhs6xrA&s"
+                    alt="githubLogo"
+                />
 
             </div>
         </div>
