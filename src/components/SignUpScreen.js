6fbbeab7ea6a315596f5c6111af9baced6d4c9be
Anup <anuprijal321@gmail.com>
2024-06-19 20:47:57 +0545

    made separate display logic for sign in and create new account

--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -1,11 +1,12 @@
 import '../styles/SignUpScreen.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
 function SignUpScreen() {
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
+    const [isCreateNew, setIsCreateNew] = useState(false);
     const navigate = useNavigate();
     const register = (e) => {
         e.preventDefault();
@@ -32,18 +33,26 @@ function SignUpScreen() {
     return (
         <div className="signup-screen">
             <form >
-                <h1>Sign In</h1>
+                {isCreateNew === false && <h1>Sign In</h1>}
+                {isCreateNew && <h1>Create an Account</h1>}
                 <input ref={emailRef} type="email"
                     placeholder="Email"
                 />
                 <input ref={passwordRef} type="password"
                     placeholder="Password"
                 />
-                <button type="submit" onClick={signIn}>Sign In</button>
-                <h4>
-                    <span className="gray-text">Don't have account? </span>
-                    <span className="signup_link" onClick={register}>Sign up now</span>
-                </h4>
+                {isCreateNew === false && <button type="submit" onClick={signIn}>Sign In</button>}
+                {isCreateNew && <button type="submit" onClick={register}>Create</button>}
+                {isCreateNew === false &&
+                    <h4>
+                        <span className="gray-text">Don't have account? </span>
+                        <span className="signup_link" onClick={() => setIsCreateNew(true)}>Sign up now</span>
+                    </h4>
+                }
+                {isCreateNew && <h4>
+                    <span className="gray-text">Already have an account? </span>
+                    <span className="signup_link" onClick={() => setIsCreateNew(false)}>Sign in</span>
+                </h4>}
             </form>
 
         </div>
